f219dc99fc5c735e3ceae8abe0b81f96a1861d01
umer-dilber <mo.umer.usman@gmail.com>
2022-12-11 12:32:36 +0500

    Working good

--- app/src/main/java/com/example/quranapp/MainActivity.java
+++ app/src/main/java/com/example/quranapp/MainActivity.java
@@ -12,24 +12,23 @@ public class MainActivity extends AppCompatActivity {
     EditText parah, ayat;
     TextView showAyat;
     Button btnSearch;
-    QuranArabicText txt;
-    QDH index;
+    QuranArabicText txt = new QuranArabicText();
+    QDH index = new QDH();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         parah = findViewById(R.id.parahNum);
         ayat = findViewById(R.id.ayatNum);
-        Integer parahNum, ayatNum;
-        parahNum = Integer.parseInt(parah.getText().toString());
-        ayatNum = Integer.parseInt(ayat.getText().toString());
+//        parahNum = Integer.parseInt(parah.getText().toString());
+//        ayatNum = Integer.parseInt(ayat.getText().toString());
         showAyat = findViewById(R.id.showAyat);
         btnSearch = findViewById(R.id.btnSearch);
-//        btnSearch.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                showAyat.setText((txt.QuranArabicText[index.getParahStart(parahNum+ayatNum)]).toString());
-//            }
-//        });
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAyat.setText(txt.QuranArabicText[index.getParahStart(Integer.parseInt(parah.getText().toString())-1)+Integer.parseInt(ayat.getText().toString())]);
+            }
+        });
     }
 }
