251ac410dc230a048af57d471d2ce526437acc1a
umer-dilber <mo.umer.usman@gmail.com>
2022-12-11 12:03:27 +0500

    not working

--- app/src/main/java/com/example/quranapp/MainActivity.java
+++ app/src/main/java/com/example/quranapp/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.quranapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText parah, ayat;
+    TextView showAyat;
+    Button btnSearch;
+    QuranArabicText txt;
+    QDH index;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        parah = findViewById(R.id.parahNum);
+        ayat = findViewById(R.id.ayatNum);
+        Integer parahNum, ayatNum;
+        parahNum = Integer.parseInt(parah.getText().toString());
+        ayatNum = Integer.parseInt(ayat.getText().toString());
+        showAyat = findViewById(R.id.showAyat);
+        btnSearch = findViewById(R.id.btnSearch);
+//        btnSearch.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                showAyat.setText((txt.QuranArabicText[index.getParahStart(parahNum+ayatNum)]).toString());
+//            }
+//        });
     }
 }
